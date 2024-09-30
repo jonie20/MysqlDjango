8155aeb424f578752a1c6ddd77348c1ae4c62953
Johnstone Kipkosgei Cheruiyot <84018132+jonie20@users.noreply.github.com>
2024-09-30 12:26:15 +0300

    Update urls.py

--- crudMysql/crudMysql/urls.py
+++ crudMysql/crudMysql/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from crudMysql import views
+from crudMysqlys import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
